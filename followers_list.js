a504839e8abfb197c3d7ee297940330a10bb967d
yc5 <yc@xstar.me>
2019-06-05 02:24:58 +0800

    Create followers_list.js

---
+++ followers_list.js
@@ -0,0 +1,116 @@
+const url = 'https://www.instagram.com/graphql/query/?'
+let query_hash = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+let variables_json = {"id":"XXXXXXXX","include_reel":true,"fetch_mutual":false,"first":100}
+let variables = encodeURIComponent(JSON.stringify(variables_json))
+
+let followers_array = []
+let after = ''
+fetch( url + 'query_hash=' + query_hash + '&variables=' + variables )
+.then(res => res.json())
+.then(json => {
+	for(const k in json.data.user.edge_followed_by.edges){
+		followers_array.push(
+			json.data.user.edge_followed_by.edges[k].node.username
+			+ ' / '
+			+ json.data.user.edge_followed_by.edges[k].node.full_name
+			+ ' / '
+			+ json.data.user.edge_followed_by.edges[k].node.id
+		)
+	}		
+	after = json.data.user.edge_followed_by.page_info.end_cursor
+	variables_json.after = after
+	variables = encodeURIComponent(JSON.stringify(variables_json))
+})
+.then(()=>{
+	fetch( url + 'query_hash=' + query_hash + '&variables=' + variables )
+	.then(res => res.json())
+	.then(json => {
+		for(const k in json.data.user.edge_followed_by.edges){
+			followers_array.push(
+				json.data.user.edge_followed_by.edges[k].node.username
+				+ ' / '
+				+ json.data.user.edge_followed_by.edges[k].node.full_name
+				+ ' / '
+				+ json.data.user.edge_followed_by.edges[k].node.id
+			)
+		}		
+		after = json.data.user.edge_followed_by.page_info.end_cursor
+		variables_json.after = after
+		variables = encodeURIComponent(JSON.stringify(variables_json))
+	})
+	.then(()=>{
+		fetch( url + 'query_hash=' + query_hash + '&variables=' + variables )
+		.then(res => res.json())
+		.then(json => {
+			for(const k in json.data.user.edge_followed_by.edges){
+				followers_array.push(
+					json.data.user.edge_followed_by.edges[k].node.username
+					+ ' / '
+					+ json.data.user.edge_followed_by.edges[k].node.full_name
+					+ ' / '
+					+ json.data.user.edge_followed_by.edges[k].node.id
+				)
+			}		
+			after = json.data.user.edge_followed_by.page_info.end_cursor
+			variables_json.after = after
+			variables = encodeURIComponent(JSON.stringify(variables_json))
+		})
+		.then(()=>{
+			fetch( url + 'query_hash=' + query_hash + '&variables=' + variables )
+			.then(res => res.json())
+			.then(json => {
+				for(const k in json.data.user.edge_followed_by.edges){
+					followers_array.push(
+						json.data.user.edge_followed_by.edges[k].node.username
+						+ ' / '
+						+ json.data.user.edge_followed_by.edges[k].node.full_name
+						+ ' / '
+						+ json.data.user.edge_followed_by.edges[k].node.id
+					)
+				}		
+				after = json.data.user.edge_followed_by.page_info.end_cursor
+				variables_json.after = after
+				variables = encodeURIComponent(JSON.stringify(variables_json))
+			})
+			.then(()=>{
+				fetch( url + 'query_hash=' + query_hash + '&variables=' + variables )
+				.then(res => res.json())
+				.then(json => {
+					for(const k in json.data.user.edge_followed_by.edges){
+						followers_array.push(
+							json.data.user.edge_followed_by.edges[k].node.username
+							+ ' / '
+							+ json.data.user.edge_followed_by.edges[k].node.full_name
+							+ ' / '
+							+ json.data.user.edge_followed_by.edges[k].node.id
+						)
+					}		
+					after = json.data.user.edge_followed_by.page_info.end_cursor
+					variables_json.after = after
+					variables = encodeURIComponent(JSON.stringify(variables_json))
+				})
+				.then(()=>{
+					fetch( url + 'query_hash=' + query_hash + '&variables=' + variables )
+					.then(res => res.json())
+					.then(json => {
+						for(const k in json.data.user.edge_followed_by.edges){
+							followers_array.push(
+								json.data.user.edge_followed_by.edges[k].node.username
+								+ ' / '
+								+ json.data.user.edge_followed_by.edges[k].node.full_name
+								+ ' / '
+								+ json.data.user.edge_followed_by.edges[k].node.id
+							)
+						}		
+						after = json.data.user.edge_followed_by.page_info.end_cursor
+						variables_json.after = after
+						variables = encodeURIComponent(JSON.stringify(variables_json))
+					})
+					.then(()=>{
+						console.log(followers_array.join('\n'))
+					})	
+				})
+			})
+		})
+	})
+})
