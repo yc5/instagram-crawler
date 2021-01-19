4787abb55ccb6cfd9bd810c50a6b1a1fdb21d446
yc5 <yc@xstar.me>
2021-01-19 10:18:49 +0800

    brand new instagram api

---
+++ list_reel_media_viewer.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import urllib.parse
+
+story_id = 'STORY_ID'
+session_id = 'SESSION_ID'
+
+my_headers = {    
+    'X-IG-App-ID': '936619743392459',
+    'Cookie': 'sessionid='+session_id
+}
+
+def list_reel_media_viewer(id, max):
+  params = {
+      'include_blacklist_sample' : 'true',
+      'max_id' : str(max)
+  }
+  r = requests.get('https://i.instagram.com/api/v1/media/'+id+'/list_reel_media_viewer/', params = params, headers = my_headers)
+  r = r.json()
+
+  for x in r['users']:
+    pk = x['pk']
+    full_name = x['full_name']
+    username = x['username']
+    is_private = x['is_private']
+
+    print('%s,%s,%s,%s' % (pk, full_name, username, is_private))
+
+  if r['next_max_id'] :
+    list_reel_media_viewer(id, r['next_max_id'])
+
+
+list_reel_media_viewer(story_id,0)
+  
